2b74ddbe8a58c3e8d95c001df2b77170b6435bf8
keshew <artyom.korotckov2017@yandex.ru>
2025-01-29 02:28:44 +0500

    Initial Commit

---
+++ Egypt/EgyptApp.swift
@@ -0,0 +1,17 @@
+//
+//  EgyptApp.swift
+//  Egypt
+//
+//  Created by Артём Коротков on 29.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct EgyptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
